75ca05ded599e4920ea8f734488d213f8ec04595
SitaaronKL <dhruvbhatia115@gmail.com>
2025-11-11 12:51:44 -0500

    Suppress troika-three-text webpack warnings in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App directory is now stable in Next.js 14, no need for experimental flag
+  webpack: (config, { isServer }) => {
+    // Suppress warnings from troika-three-text dependencies
+    // These are harmless warnings from node_modules that don't affect functionality
+    config.ignoreWarnings = [
+      {
+        module: /node_modules\/troika-three-text/,
+      },
+      {
+        message: /does not contain a default export/,
+      },
+      /webgl-sdf-generator/,
+      /bidi-js/,
+    ];
+
+    return config;
+  },
 }
 
 module.exports = nextConfig 
